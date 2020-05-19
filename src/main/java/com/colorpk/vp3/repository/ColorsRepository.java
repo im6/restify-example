f5074b59798a2f7578875b97bae3414d569af5c3
im6 <ee2win@gmail.com>
2020-05-19 01:05:55 +0000

    revert

--- src/main/java/com/colorpk/vp3/repository/ColorsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.colorpk.vp3.repository;
-
-import com.colorpk.vp3.models.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColorsRepository extends JpaRepository<Color, Integer> {
-}
