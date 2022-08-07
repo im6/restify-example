83cfd28ce3899285b4cbc98c00bf05c3de343138
im6 <ee2win@gmail.com>
2022-08-07 11:17:18 -0400

    upgrade to go 1.18

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.SetTrustedProxies([]string{"127.0.0.1"}) // reverse proxy ip address
 	handler.Initialize(r)
 	r.Run(":" + config.GetPort())
 }
